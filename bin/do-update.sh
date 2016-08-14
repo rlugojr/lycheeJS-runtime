cee6e5b188c07e74a5d3bdfef97d7f7b74189a5c
Cookie Engineer <cookiengineer@protonmail.ch>
2016-08-14 03:23:09 +0200

    :bug: Fix for do-update.sh

--- bin/do-update.sh
+++ bin/do-update.sh
@@ -9,8 +9,8 @@ RUNTIME_ROOT=$(cd "$(dirname "$0")/../"; pwd);
 
 for runtime in $RUNTIME_ROOT/* ; do
 
-	if [[ -f ./$runtime/update.sh ]]; then
-		./$runtime/update.sh;
+	if [ -f $runtime/update.sh ]; then
+		$runtime/update.sh;
 	fi;
 
 done;
